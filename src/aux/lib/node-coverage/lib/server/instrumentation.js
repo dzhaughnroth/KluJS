480b3154dc2df9ddad3528f1c3d28745cf963ab4
dzhaughnroth <dzhaughnroth@sourceforge.net>
2012-06-21 23:13:52 -0700

    node coverage added; was a git repo before oops

---
+++ src/aux/lib/node-coverage/lib/server/instrumentation.js
@@ -0,0 +1,76 @@
+var express = require("express"), fs = require("fs");
+
+var report = require("../report");
+var instrument = require("../instrument");
+var common = require("./common");
+var fileSystem = require("../fileSystem");
+
+var sourceCodeCache = {
+	code : {},
+	highlight : {}
+};
+
+exports.start = function (docRoot, port, adminRoot, adminPort, coverageOptions) {
+	var app = express.createServer();
+
+	app.use(express.bodyParser());
+	app.use(express.cookieParser());
+	app.use(express.session({secret: "theres not much secrecy here"}));
+
+
+	app.get("/*.js", function (req, res) {
+		var instrumentedCode = sourceCodeCache[req.url];
+
+		if (instrumentedCode) {
+			res.send(instrumentedCode.clientCode, {"Content-Type" : "text/javascript"});
+		} else {
+			fs.readFile(docRoot + req.url, "utf-8", function (err, content) {
+				if (err) {
+					res.send("Error while reading " + req.url + err, 500);
+				} else {
+					var code = instrument(req.url, content, coverageOptions);
+					sourceCodeCache.code[req.url] = code.clientCode;
+
+					if (!coverageOptions.doHighlight) {
+						sourceCodeCache.highlight[req.url] = code.highlightedCode;
+						req.session.highlightInMemory = true;
+					}
+
+					res.send(code.clientCode, {"Content-Type" : "text/javascript"});
+				}
+			});
+		}
+	});
+
+	app.post("/node-coverage-store", function (req, res) {
+		mergeCodeFromMemory(req.body.code, sourceCodeCache.highlight);
+		
+		common.saveCoverage(req.body, adminRoot, function (error) {
+			if (error) {
+				res.send(error, 500);
+			} else {
+				res.send(200);
+			}
+		});
+	});
+
+	app.post("/*", function (req, res, next) {
+		res.sendfile(docRoot + req.url);
+	});
+
+	app.use(express.static(docRoot));
+
+	app.listen(port);
+};
+
+function mergeCodeFromMemory (inside, from) {
+	if (!inside) {
+		return;
+	}
+
+	for (var fileName in inside) {
+		if (!inside[fileName].src) {
+			inside[fileName] = from[fileName];
+		}
+	}
+}
