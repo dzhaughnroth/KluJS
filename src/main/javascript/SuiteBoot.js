63cf20da269cf3e01a2848605baa3f467e415f50
dzhaughnroth <dzhaughnroth@sourceforge.net>
2012-08-07 21:51:33 -0700

    Adjust args to SuiteStarter call to include requirejs

--- src/main/javascript/SuiteBoot.js
+++ src/main/javascript/SuiteBoot.js
@@ -1,8 +1,8 @@
-/*globals define:false, jasmine:false, klujsAssembly:true, requireJs:false */
+/*globals define:false, jasmine:false, klujsAssembly:true, requirejs:false */
 
 define( [ "./lib/order!./lib/jasmine", "./lib/order!./lib/jasmine-html", "./lib/order!./SuiteStarter", "./lib/order!./HtmlPageFacade" ], function( j, jh, SuiteStarter, HtmlPageFacade) {
     
-    var ss = new SuiteStarter( new HtmlPageFacade(), jasmine, requireJs );
+    var ss = new SuiteStarter( new HtmlPageFacade(), jasmine, null, requirejs );
     klujsAssembly = ss.suitePage.assembly;
     ss.start();
 } );
