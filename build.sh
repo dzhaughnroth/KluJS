a60493de67c64d14d718a8b03c96e823c1cad592
dzhaughn <dzhaughn@yahoo.com>
2013-06-05 11:29:14 -0700

    reset author

--- build.sh
+++ build.sh
@@ -4,10 +4,10 @@ mkdir build/example/
 mkdir build/example/KluJS
 mkdir build/klujsCopy
 mkdir build/klujsCopy/KluJS
-cp -r src/main/javascript build/example/KluJS/
-cp -r src/aux/* build/example/KluJS
+cp -r src/main/js build/example/KluJS/
+cp -r src/node/* build/example/KluJS
 cp -r src/example/* build/example
-cp -r src/main/javascript build/klujsCopy/KluJS/
-cp -r src/aux/* build/klujsCopy/KluJS
+cp -r src/main/js build/klujsCopy/KluJS/
+cp -r src/node/* build/klujsCopy/KluJS
 cp -r src build/klujsCopy
 cp -r klujs-config.js build/klujsCopy
